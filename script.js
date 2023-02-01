ff5cc10766b7b007b21433e7a7f9218ee81ce7a2
Prajapati-Shivam <prajapatishivam11203@gmail.com>
2023-02-01 19:56:02 +0530

    removed easter egg

--- script.js
+++ script.js
@@ -1,42 +1,53 @@
 const weather = {
-  apikey: '98eb1f9a9a72f2becf0db8700c1dae4b',
-  fetchWeather: function(city) {
+  apikey: "98eb1f9a9a72f2becf0db8700c1dae4b",
+  fetchWeather: function (city) {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q="
-      + city + "&units=metric&appid="
-      + this.apikey
-      ).then((response) => response.json())
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=metric&appid=" +
+      this.apikey
+    )
+      .then((response) => response.json())
       .then((data) => this.displayWeather(data))
+      .catch((err) => {
+        JSAlert.alert("Please enter a valid city name");
+      });
   },
-  displayWeather: function(data) {
+  displayWeather: function (data) {
     const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
 
     document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".icon").src = "http://openweathermap.org/img/wn/"+ icon + ".png";
+    document.querySelector(".icon").src =
+      "http://openweathermap.org/img/wn/" + icon + ".png";
 
     document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°C";
-    document.querySelector(".humidity").innerText = "Humidity " + humidity + "%";
-    document.querySelector(".wind").innerText = "Wind speed: "+ speed + "km/h";
+    document.querySelector(".humidity").innerText =
+      "Humidity " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + "km/h";
     document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?+" + name + "')";
+    document.body.style.backgroundImage =
+      `url('https://source.unsplash.com/${screen.width}x${screen.height}/?${name}')`;
   },
-  search: function() {
-    this.fetchWeather(document.querySelector('.search-bar').value);
-  }
-}
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
 
-document.querySelector('.search button').addEventListener("click", function() {
+document.querySelector(".search button").addEventListener("click", function () {
   weather.search();
-})
+});
 
-document.querySelector('.search-bar').addEventListener("keyup", function(event) {
-  if (event.key == "Enter") {
-    weather.search();
-  }
-})
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
 
-weather.fetchWeather('Mumbai')
+weather.fetchWeather("Mumbai");
